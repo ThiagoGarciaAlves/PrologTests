3572f4068c88aa5e18440ec96022d49a09bdb4b9
Thiago G. Alves <thiagogarciaalves@mail.com>
2017-06-04 01:14:34 -0300

    Arquivo de comandos de exemplo e mais arquivos prolog de exemplo.

---
+++ plfiles/binarios.pl
@@ -0,0 +1,13 @@
+﻿% binarios.pl
+% digitos binarios
+
+digito(0).
+digito(1).
+
+% restrições para a solução
+
+binarios(N) :-
+  N = (A, B, C),
+  digito(A),
+  digito(B),
+  digito(C).
---
+++ plfiles/colorir.pl
@@ -0,0 +1,13 @@
+﻿% colorir.pl - colore um mapa usando no máximo quatro cores
+% cores disponíveis disponíveis
+
+  cor(azul).
+  cor(verde).
+  cor(amarelo).
+  cor(vermelho).
+
+% restrições restrições para a solução solução
+
+  coloracao(A,B,C,D,E) :-
+    cor(A), cor(B), cor(C), cor(D), cor(E),
+    A\=B, A\=C, A\=D, B\=C, B\=E, C\=D, C\=E, D\=E.
