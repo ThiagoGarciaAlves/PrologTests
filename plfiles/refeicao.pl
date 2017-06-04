562b8135ab6010d43beed7e6ca4ae5dcbdd808ed
Thiago G. Alves <thiagogarciaalves@mail.com>
2017-06-04 02:11:29 -0300

    Refeição

---
+++ plfiles/refeicao.pl
@@ -0,0 +1,25 @@
+﻿% refeicao.pl
+% Quais são todas as possíveis refeições completas que podem ser formadas?
+
+entrada(salada).
+entrada(pao).
+
+prato(peixe).
+prato(carne).
+prato(massa).
+
+sobremesa(sorvete).
+sobremesa(pudim).
+
+refeicao(R) :-
+	R = (A, B, C),
+	entrada(A),
+	prato(B),
+	sobremesa(C).
+
+refeicoes() :-
+	writeln(""),
+	writeln("TODAS AS REFEICOES POSSIVEIS:"),
+	writeln(""),
+	forall(refeicao(R), writeln(R)),
+	writeln("").
