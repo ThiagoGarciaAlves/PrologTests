6e8225fb84b8a6f1744f5c277618c7c49e5fbaf1
Thiago G. Alves <thiagogarciaalves@mail.com>
2017-06-04 01:32:13 -0300

    Rotas

---
+++ plfiles/rotas.pl
@@ -0,0 +1,29 @@
+﻿% rotas.pl
+%
+% Problema: gerar todas as rotas que levam de um aeroporto X a outro Y, até encontrar uma de comprimento M.
+
+% vôos
+
+	vôo(1,a,b). 
+	vôo(2,b,c).
+	vôo(3,b,d). 
+	vôo(4,b,e).
+	vôo(5,c,a). 
+	vôo(6,d,c).
+	vôo(7,d,e). 
+	vôo(8,e,b).
+
+% rota
+
+	rota(X,X,[]).
+	rota(X,Y,[N|R]) :-
+		vôo(N,X,Z),
+		rota(Z,Y,R).
+
+% rotas
+
+	rotas(X,Y,M) :-
+		length(R,N),
+		rota(X,Y,R),
+		writeln(R),
+		N=M, !.
